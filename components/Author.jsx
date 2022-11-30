448585570f35c0137e57d430199c09d62a714f8a
Tom Hestamp <tom@hestamp.me>
2022-11-30 02:00:10 +0200

    quotes=)

--- components/Author.jsx
+++ components/Author.jsx
@@ -14,7 +14,7 @@ const Author = ({ author }) => {
         />
       </div>
       <h3 className="text-white my-2 text-xl font-bold">{author.name}</h3>
-      <p className="text-white text-lg">''{author.bio}''</p>
+      <p className="text-white text-lg">{author.bio}</p>
     </div>
   )
 }
